c666fed99ef04abd378ac8a2931f139d40231362
Alejandro Arriaga <19030837@itcelaya.edu.mx>
2023-10-06 15:39:56 -0600

    Base de rutas con json

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Return_;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +24,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/contact', fn () => Response::view('contact'));
+
+Route::post('/contact', function (Request $request) {
+    return Response::json(["message" => "hola"])->setStatusCode(400);
+});
+
+Route::get('/change-password', fn () => Response::view('change-password'));
